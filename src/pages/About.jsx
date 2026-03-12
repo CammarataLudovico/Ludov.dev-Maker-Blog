fe18a23edc1c46d460cecc7900ed7f607680fe1a
CammarataLudovico <cammarataludvico9@gmail.com>
2026-03-12 14:36:18 +0100

    feat: add profile image to About page, update skills section formatting, and enhance CSS for profile image responsiveness

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import profile from "../assets/img/squadra12.JPG";
 
 const lines = [
   "Hi.",
   "I'm Ludovico Cammarata, a Front-End Developer based in Italy.",
   "I study and work with web development every day, but I'm also passionate about back-end development like Node.js & Laravel.",
+  "I'm also passionate about 3d printing (I own a Bambu Lab A1 Combo), IoT, and DevOps tools like Docker, Grafana, and Prometheus.",
   "",
   "I mainly work with React, Laravel, and Node.js.",
   "I'm constantly learning and building real projects to improve my skills.",
+  "I have my own VPS where I host my projects and experiment with new technologies and tools.",
   "",
   "You can find my projects on GitHub @CammarataLudovico.",
   "When I'm not coding, I train, design, and work on personal ideas.",
@@ -19,7 +22,10 @@ export default function About() {
   return (
     <div>
       <div id="title">
-        <a href="/">&lt;</a> About
+        <a href="/">&lt;</a> About 
+      </div>
+      <div>
+        <img className="profile-img" src={profile} alt="Profile" />
       </div>
       {lines.map((text, i) => (
         text === "" ? (
@@ -32,7 +38,7 @@ export default function About() {
       <span className="empty-line" />
       <span className="status-line">
         <span>.quit</span>
-        <span>03.02.2026        CammarataLudovico</span>
+        <span>12.03.2026        CammarataLudovico</span>
       </span>
     </div>
   );
